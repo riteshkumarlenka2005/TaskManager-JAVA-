93e1d84be3f6f517b808f84ff88e247853c48cd1
Ritesh Kumar Lenka <lenkariteshkumar2005@gmail.com>
2026-03-25 01:30:09 +0530

    Fix Infinite JSON Recursion: Add JsonIgnore to Task-User relationship

--- server/src/main/java/com/taskmanager/api/model/Task.java
+++ server/src/main/java/com/taskmanager/api/model/Task.java
@@ -39,6 +39,7 @@ public class Task {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
+    @com.fasterxml.jackson.annotation.JsonIgnore
     private User user;
 
     @PrePersist
